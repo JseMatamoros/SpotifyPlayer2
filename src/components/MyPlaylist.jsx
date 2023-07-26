776ba9c4b65913a5aaa2827e256524b5f32750ae
AryunDev <josematamoros916@gmail.com>
2023-07-26 17:11:09 -0400

    Commit inicial con plantilla vista en clases

---
+++ src/components/MyPlaylist.jsx
@@ -0,0 +1,36 @@
+/**
+* Componente fusion entre Title y Form
+*/
+import { SpotifyPlayer } from "./SpotifyPlayer";
+
+const playlist = [
+    { url: '33qkK1brpt6t8unIpeM2Oy', favorite: true },
+    { url: '0H6TddUF2M63ZSHGvhk5yy', favorite: true },
+    { url: '3fn4HfVz5dhmE0PG24rh6h', favorite: true },
+    { url: '0DQyTVcDhK9wm0f6RaErWO', favorite: true }
+];
+export const MyPlaylist = () => {
+    return (
+        <div className='container'>
+            <h1 className='title text-center mt-5'>My favorite songs</h1>
+            <div className='d-flex align-items-center'>
+                <input type='text' className='form-control' placeHolder='Ingrese el codigo del album aqui...'></input>
+                <div className='form-check ms-2'>
+                    <input className='form-check-input' type='checkbox' />
+                    <label className='form-check-label'>
+                        Favorite
+                    </label>
+                </div>
+                <button className='btn btn-success ms-2'><i class="bi bi-plus-circle-fill"></i></button>
+                <button className='btn btn-danger ms-2'><i class="bi bi-trash"></i></button>
+            </div>
+            <div>
+                {
+                    playlist.map(song => <SpotifyPlayer url={song.url} favorite={song.favorite} />)
+                }
+            </div>
+
+        </div>
+    )
+}
+
