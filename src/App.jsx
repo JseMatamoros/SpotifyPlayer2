6fd5e6b4c5d689abc931b5da1b0f241dce086937
AryunDev <josematamoros916@gmail.com>
2023-07-26 17:35:00 -0400

    comit cambios otra vez

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import { MyPlaylist } from "./components/MyPlaylist"
-/**
-* Nuestro componente App sera el principal y el que sera renderizado en index.js
-*/
-export const App = () => <MyPlaylist/>
-
-
